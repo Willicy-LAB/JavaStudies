a4dfecea7d92c32129cd06c27d2dafb5913d2279
Willicy-LAB <v2lsbfryzwf0eq@tuta.io>
2025-01-15 11:53:08 -0300

    sending the directory source/.

---
+++ src/main/java/com/dev/will/Main.java
@@ -0,0 +1,12 @@
+package com.dev.will;
+
+import org.jetbrains.annotations.*;
+public class Main {
+  public static void main(String[] args) {
+    try {
+      
+    } catch (@NotNull Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+}
