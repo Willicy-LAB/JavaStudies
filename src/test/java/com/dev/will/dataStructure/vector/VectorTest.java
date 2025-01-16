bb758fb94917ed768ccac3e2a17056e7d4702c6b
Willicy-LAB <v2lsbfryzwf0eq@tuta.io>
2025-01-16 19:29:27 -0300

    studying vectors: implemented comprehensive tests for the  method to ensure correct behavior in various scenarios, including adding single and multiple elements, handling capacity limits, and testing null input.

--- src/test/java/com/dev/will/dataStructure/vector/VectorTest.java
+++ src/test/java/com/dev/will/dataStructure/vector/VectorTest.java
@@ -1,17 +1,61 @@
 package com.dev.will.dataStructure.vector;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 public class VectorTest {
 
+  //testing vector creation
   @Test
-  public void getTosize() {
+  void getTosize() {
     @NotNull Vector vector = new Vector(100);
 
     @NotNull String[] myStrings = vector.getElements();
     assertEquals(100, myStrings.length);
+  }
+
+  @Test
+  void testAddSingleElement() {
+    @NotNull Vector vector = new Vector(3);
+    vector.add("Element1");
+
+    assertEquals("Element1", vector.getElements()[0]);
+  }
+
+  @Test
+  void testAddMultipleElements() {
+    @NotNull Vector vector = new Vector(3);
+    vector.add("Element1");
+    vector.add("Element2");
+    vector.add("Element3");
+
+    assertEquals("Element1", vector.getElements()[0]);
+    assertEquals("Element2", vector.getElements()[1]);
+    assertEquals("Element3", vector.getElements()[2]);
+  }
+
+  @Test
+  void testAddExceedingCapacity() {
+    @NotNull Vector vector = new Vector(2);
+    vector.add("Element1");
+    vector.add("Element2");
+    vector.add("Element3");
+
+    assertEquals("Element1", vector.getElements()[0]);
+    assertEquals("Element2", vector.getElements()[1]);
+    assertNull(vector.getElements()[2], "Exceeding capacity should not add new elements.");
+  }
+
+  @Test
+  void testAddNullElement() {
+    @NotNull Vector vector = new Vector(2);
+    vector.add("Element1");
+    vector.add(null);
+
+    assertEquals("Element1", vector.getElements()[0]);
+    assertNull(vector.getElements()[1]);
   }
 }
