49ca19f4e33a06b7f02caf6532c00ce47d175756
Willicy-LAB <v2lsbfryzwf0eq@tuta.io>
2025-01-18 10:41:09 -0300

    add method to insert an element into the array with bounds checking

--- src/main/java/com/dev/will/dataStructure/vector/Vector.java
+++ src/main/java/com/dev/will/dataStructure/vector/Vector.java
@@ -5,15 +5,18 @@ import org.jetbrains.annotations.NotNull;
 public class Vector {
 
   private @NotNull String[] elements;
+  private int size;
 
   public Vector (int capacity) {
     this.elements = new String[capacity];
+    this.size = 0;
   }
 
   public @NotNull String[] getElements () {
     return elements;
   }
 
+  /* first version of method add
   public void add (@NotNull String element) {
     for (int i = 0; i < this.elements.length; i++) {
       if (this.elements[i] == null) {
@@ -21,6 +24,20 @@ public class Vector {
         return;
       }
     }
+
     throw new ArrayIndexOutOfBoundsException("no space available to add more elements");
   }
-}
+  */
+
+  // second version of method add
+  public void add(@NotNull String element) {
+    if (this.size < this.elements.length) {
+      this.elements[this.size] = element;
+      this.size = this.size + 1;
+
+      return;
+    }
+
+    throw new ArrayIndexOutOfBoundsException("no space available to add more elements");
+  }
+ }
