51e0b2fa32beee07d99a86b7a6c47e12e40c2244
Willicy-LAB <v2lsbfryzwf0eq@tuta.io>
2025-01-20 20:43:03 -0300

    studying vectors: reorganize and enhance Vector test structure for improved clarity and completeness

--- src/test/java/com/dev/will/dataStructure/vector/VectorTest.java
+++ src/test/java/com/dev/will/dataStructure/vector/VectorTest.java
@@ -1,180 +1,114 @@
 package com.dev.will.dataStructure.vector;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.Test;
 
 public class VectorTest {
 
-  //testing vector creation
-  @Test
-  void getTosize () {
-    @NotNull Vector vector = new Vector(100);
+  private @NotNull Vector vector;
 
-    @NotNull String[] myStrings = vector.getElements();
-    assertEquals(100, myStrings.length);
+  @BeforeEach
+  void setup() {
+    vector = new Vector(3);
   }
 
   @Test
-  void testAddSingleElement () {
-    @NotNull Vector vector = new Vector(3);
-    vector.add("Element1");
-
-    assertEquals("Element1", vector.getElements()[0]);
+  void testAddElement() {
+    assertTrue(vector.add("Element1"));
+    assertTrue(vector.add("Element2"));
+    assertEquals(2, vector.getSize());
   }
 
   @Test
-  void testAddMultipleElements () {
-    @NotNull Vector vector = new Vector(3);
+  void testAddElementWithCapacityIncrease() {
     vector.add("Element1");
     vector.add("Element2");
     vector.add("Element3");
-
-    assertEquals("Element1", vector.getElements()[0]);
-    assertEquals("Element2", vector.getElements()[1]);
-    assertEquals("Element3", vector.getElements()[2]);
+    assertTrue(vector.add("Element4"));
+    assertEquals(4, vector.getSize());
   }
 
-  /* off
+  @Test
+  void testAddElementAtIndex() {
+    vector.add("Element1");
+    vector.add("Element3");
+    vector.add(1, "Element2");
+    assertEquals("[Element1, Element2, Element3]", vector.toString());
+  }
 
   @Test
-  void testAddExceedingCapacity () {
-    @NotNull Vector vector = new Vector(2);
+  void testAddElementAtIndexInvalid() {
+    vector.add("Element1");
+    assertThrows(IllegalArgumentException.class, () -> vector.add(-1, "Invalid"));
+    assertThrows(IllegalArgumentException.class, () -> vector.add(2, "Invalid"));
+  }
+
+  @Test
+  void testGetElementByIndex() {
+    vector.add("Element1");
+    vector.add("Element2");
+    assertEquals("Element1", vector.getElementByIndex(0));
+    assertEquals("Element2", vector.getElementByIndex(1));
+  }
+
+  @Test
+  void testGetElementByIndexInvalid() {
+    vector.add("Element1");
+    assertThrows(IllegalArgumentException.class, () -> vector.getElementByIndex(-1));
+    assertThrows(IllegalArgumentException.class, () -> vector.getElementByIndex(2));
+  }
+
+  @Test
+  void testContains() {
+    vector.add("Element1");
+    vector.add("Element2");
+    assertTrue(vector.contains("Element1"));
+    assertFalse(vector.contains("Element3"));
+  }
+
+  @Test
+  void testRemoveElement() {
     vector.add("Element1");
     vector.add("Element2");
     vector.add("Element3");
-
-    assertEquals("Element1", vector.getElements()[0]);
-    assertEquals("Element2", vector.getElements()[1]);
-    assertNull(vector.getElements()[2], "exceeding capacity should not add new elements");
+    vector.remove(1);
+    assertEquals(2, vector.getSize());
+    assertEquals("[Element1, Element3]", vector.toString());
   }
-  */
-
-  /* off
 
   @Test
-  void testAddNullElement () {
-    @NotNull Vector vector = new Vector(2);
+  void testRemoveElementInvalid() {
     vector.add("Element1");
-    vector.add(null);
-
-    assertEquals("Element1", vector.getElements()[0]);
-    assertNull(vector.getElements()[1]);
+    assertThrows(IllegalArgumentException.class, () -> vector.remove(-1));
+    assertThrows(IllegalArgumentException.class, () -> vector.remove(1));
   }
-  */
 
   @Test
-  void testReadAllElements () {
-    @NotNull Vector vector = new Vector(5);
+  void testToStringEmptyVector() {
+    assertEquals("[]", vector.toString());
+  }
 
+  @Test
+  void testToStringWithElements() {
+    vector.add("Element1");
+    vector.add("Element2");
+    assertEquals("[Element1, Element2]", vector.toString());
+  }
+
+  @Test
+  void testMoreCapacity() {
     vector.add("Element1");
     vector.add("Element2");
     vector.add("Element3");
-
-    assertEquals("Element1", vector.getElements()[0]);
-    assertEquals("Element2", vector.getElements()[1]);
-    assertEquals("Element3", vector.getElements()[2]);
-
-    System.out.println(vector.toString());
-  }
-
-  @Test
-  void testGetElementByIndex () {
-    @NotNull Vector vector = new Vector(3);
-
-    vector.add("Element1");
-    vector.add("Element2");
-    vector.add("Element3");
-
-    assertEquals("Element3", vector.getElementByIndex(2));
-    System.out.println(vector.getElementByIndex(2));
-  }
-
-  /* off
-
-  @Test
-  void testExceptionOfGetElementByIndex () {
-    @NotNull Vector vector = new Vector(3);
-
-    vector.add("Element1");
-    vector.add("Element2");
-    vector.add("Element3");
-
-    assertEquals("Element4", vector.getElementByIndex(3));
-  }
-  */
-
-  @Test
-  void testContainsElement () {
-    @NotNull Vector vector = new Vector(3);
-
-    vector.add("Element1");
-    vector.add("Element2");
-    vector.add("Element3");
-
-    assertEquals(true, vector.contains("ELEMENT1"));
-  }
-
-  @Test
-  void testAddByIndex () {
-    @NotNull Vector vector = new Vector(10);
-
-    vector.add("B");
-    vector.add("C");
-    vector.add("D");
-    vector.add("F");
-    vector.add("G");
-    vector.add("H");
-    System.out.println(vector.toString());
-
-    vector.add(0, "A");
-    System.out.println(vector.toString());
-
-    vector.add(4, "E");
-    System.out.println(vector.toString());
-  }
-
-  @Test
-  void testMoreCapacity () {
-    @NotNull Vector vector = new Vector(3);
-
-    vector.add("B");
-    vector.add("C");
-    vector.add("D");
-    System.out.println(vector.toString());
-    System.out.println(vector.getElements().length);
-
-    vector.add("F");
-    vector.add("G");
-    System.out.println(vector.toString());
-    System.out.println(vector.getElements().length);
-
-    vector.add(0, "A");
-    System.out.println(vector.toString());
-
-    vector.add(4, "E");
-    System.out.println(vector.toString());
-
-    System.out.println(vector.getElements().length);
-  }
-
-  @Test
-  void testRemove () {
-    @NotNull Vector vector = new Vector(3);
-
-    vector.add("A");
-    vector.add("B");
-    vector.add("C");
-    vector.add("D");
-    vector.add("E");
-    vector.add("F");
-
-    System.out.println(vector.toString());
-
-    vector.remove(0);
-    
-    System.out.println(vector.toString());
-    System.out.println(vector.getSize());
+    vector.add("Element4");
+    assertEquals(4, vector.getSize());
+    assertEquals("[Element1, Element2, Element3, Element4]", vector.toString());
   }
 }
