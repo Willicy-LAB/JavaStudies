914610918b974a56a2caa3b395427ec4d2c51e66
Willicy-LAB <v2lsbfryzwf0eq@tuta.io>
2025-01-20 20:29:26 -0300

    studying vectors: reorganized methods for better readability and logical flow

--- src/main/java/com/dev/will/dataStructure/vector/Vector.java
+++ src/main/java/com/dev/will/dataStructure/vector/Vector.java
@@ -7,43 +7,12 @@ public class Vector {
   private @NotNull String[] elements;
   private int size;
 
-  public Vector (int capacity) {
+  public Vector(int capacity) {
     this.elements = new String[capacity];
     this.size = 0;
   }
 
-  public @NotNull String[] getElements () {
-    return elements;
-  }
-
-  /* first version of method add
-  public void add (@NotNull String element) {
-    for (int i = 0; i < this.elements.length; i++) {
-      if (this.elements[i] == null) {
-        this.elements[i] = element;
-        return;
-      }
-    }
-
-    throw new ArrayIndexOutOfBoundsException("no space available to add more elements");
-  }
-  */
-
-  /* second version of method add
-  public void add (@NotNull String element) {
-    if (this.size < this.elements.length) {
-      this.elements[this.size] = element;
-      this.size = this.size + 1;
-
-      return;
-    }
-
-    throw new ArrayIndexOutOfBoundsException("no space available to add more elements");
-  }
-  */
-
-  // same method add v2 but with a little modification
-   public boolean add (@NotNull String element) {
+  public boolean add(@NotNull String element) {
     moreCapacity();
     if (this.size < this.elements.length) {
       this.elements[this.size] = element;
@@ -51,51 +20,10 @@ public class Vector {
 
       return true;
     }
-
     return false;
   }
 
-  public int getSize () {
-    return this.size;
-  }
-
-  public @NotNull String toString () {
-    @NotNull StringBuilder stringBuilder = new StringBuilder();
-
-    stringBuilder.append("[");
-
-    for (int i = 0; i < this.size - 1; i++) {
-      stringBuilder.append(this.elements[i]);
-      stringBuilder.append(", ");
-    }
-
-    if (this.size > 0) {
-      stringBuilder.append(this.elements[this.size-1]);
-    }
-
-    stringBuilder.append("]");
-
-    return stringBuilder.toString();
-  }
-
-  public @NotNull String getElementByIndex (int index) {
-    if (!(index >= 0 && index < this.size)) {
-      throw new IllegalArgumentException("invalid index");
-    }
-    return this.elements[index];
-  }
-
-  public boolean contains (@NotNull String element) {
-    for (int i = 0; i < this.size; i++) {
-      if (this.elements[i].equalsIgnoreCase(element)) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  public void  add (int index, @NotNull String element) {
+  public void add(int index, @NotNull String element) {
     moreCapacity();
     if (!(index >= 0 && index < this.size)) {
       throw new IllegalArgumentException("invalid index");
@@ -109,19 +37,7 @@ public class Vector {
     this.size = this.size + 1;
   }
 
-  private void moreCapacity () {
-    if (this.size == this.elements.length) {
-      @NotNull String[] newElements = new String[this.elements.length * 2];
-      
-      for (int i = 0; i < this.elements.length; i++) {
-        newElements[i] = this.elements[i];
-      }
-
-      this.elements = newElements;
-    }
-  }
-
-  public void remove (int index) {
+  public void remove(int index) {
     if (!(index >= 0 && index < this.size)) {
       throw new IllegalArgumentException("invalid index");
     }
@@ -132,4 +48,58 @@ public class Vector {
 
     this.size = this.size - 1;
   }
- }
+
+  public @NotNull String getElementByIndex(int index) {
+    if (!(index >= 0 && index < this.size)) {
+      throw new IllegalArgumentException("invalid index");
+    }
+    return this.elements[index];
+  }
+
+  public boolean contains(@NotNull String element) {
+    for (int i = 0; i < this.size; i++) {
+      if (this.elements[i].equalsIgnoreCase(element)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private void moreCapacity() {
+    if (this.size == this.elements.length) {
+      @NotNull String[] newElements = new String[this.elements.length * 2];
+
+      for (int i = 0; i < this.elements.length; i++) {
+        newElements[i] = this.elements[i];
+      }
+
+      this.elements = newElements;
+    }
+  }
+
+  public @NotNull String[] getElements() {
+    return elements;
+  }
+
+  public int getSize() {
+    return this.size;
+  }
+
+  public @NotNull String toString() {
+    @NotNull StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append("[");
+
+    for (int i = 0; i < this.size - 1; i++) {
+      stringBuilder.append(this.elements[i]);
+      stringBuilder.append(", ");
+    }
+
+    if (this.size > 0) {
+      stringBuilder.append(this.elements[this.size - 1]);
+    }
+
+    stringBuilder.append("]");
+    return stringBuilder.toString();
+  }
+}
+
